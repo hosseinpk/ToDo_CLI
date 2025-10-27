78290f8e5290e6268d177c83e88964aaac1a35b3
hossein <hosseinpakaein@gmail.com>
2025-10-27 15:23:54 +0000

    add logging

--- todo.py
+++ todo.py
@@ -1,7 +1,17 @@
 import click
-import sqlite3
+import logging
 from pathlib import Path
 from create import create_db
+
+
+BASE_DIR = Path.home() / ".todo"
+LOG_FILE = BASE_DIR / "todo.log"
+logging.basicConfig(
+    
+    filename=LOG_FILE,
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 @click.group()
@@ -27,17 +37,21 @@ def create(name:str , force:bool) -> str:
     if db_path.exists():
         if not force:
             click.secho(f" Database '{name}.db' already exists.", fg="yellow")
+            logging.warning(f" Database '{name}.db' already exists.")
             click.secho("   Use --force to overwrite it.", fg="yellow")
             raise click.Abort()
         else:
             click.echo(f"remove existing file {name}")
+            logging.warning(f"remove existing file {name}")
             db_path.unlink()
             
     try:
         create_db(name)
         click.secho(f" {name}.db created successfully in ~/.todo", fg="green")
+        logging.info(f" {name}.db created successfully in ~/.todo")
     except Exception as e:
         click.secho(f" Error creating database: {e}", fg="red")
+        logging.error(f" Error creating database: {e}")
         raise click.Abort()
 
 if __name__ == "__main__":
