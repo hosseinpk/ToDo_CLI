41e98f79f5864718fd2eccc730b3f15c62f57e24
hossein <hosseinpakaein@gmail.com>
2025-10-27 15:31:31 +0000

    correct time format

--- todo.py
+++ todo.py
@@ -10,7 +10,8 @@ logging.basicConfig(
     
     filename=LOG_FILE,
     level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
 )
 
 
